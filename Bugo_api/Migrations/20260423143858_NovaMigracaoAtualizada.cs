fa74fdc2a3e3ad10bcd9441b2e52d2746dcfc31f
David Salgado Analista DevOps Jr. <david.salgado@formacerta.com.br>
2026-04-24 14:42:30 -0300

    criação do painel de usuario

---
+++ Bugo_api/Migrations/20260423143858_NovaMigracaoAtualizada.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bugo_blazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovaMigracaoAtualizada : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Prioridade",
+                table: "Chamados",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Prioridade",
+                table: "Chamados");
+        }
+    }
+}
